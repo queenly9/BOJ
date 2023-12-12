dd0d8ba07e77790cb11fd1c57df5e437c1c90f09
queenly9 <queenly9@naver.com>
2023-12-12 10:08:08 +0900

    실버 드가자

---
+++ 2754.py
@@ -0,0 +1,28 @@
+str = input()
+if str == 'A+' :
+    print(4.3)
+elif str == 'A0':
+    print(4.0)
+elif str == 'A-':
+    print(3.7)
+elif str == 'B+':
+    print(3.3)
+elif str == 'B0':
+    print(3.0)
+elif str == 'B-':
+    print(2.7)
+elif str == 'C+':
+    print(2.3)
+elif str == 'C0':
+    print(2.0)
+elif str == 'C-':
+    print(1.7)
+elif str == 'D+':
+    print(1.3)
+elif str == 'D0':
+    print(1.0)
+elif str == 'D-':
+    print(0.7)
+elif str == 'F':
+    print(0.0)
+
